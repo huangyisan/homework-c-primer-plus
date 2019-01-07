1b7c1aad6e1203274237a54f1750ebee53fd5802
huangyisan <ahuangyisan@live.cn>
2019-01-07 22:47:52 +0800

    update

---
+++ chapter3/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main(void) {
+    float a;
+    float b;
+    int c;
+    a = 1;
+    b = 1.0;
+    c = 1;
+
+    printf("the num a is %f \n", a);
+    printf("the num a is %d \n", b);
+    printf("the num a is %f \n", c);
+}
+
---
+++ chapter3/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main(void) {
+    int a;
+    scanf("%d", &a);
+    printf("the input ascii to char is %c \n", a);
+}
+
