be714b6f00f10329d9299fb73aff57e0a8fd9288
huangyisan <ahuangyisan@live.cn>
2019-01-23 22:54:30 +0800

    题目1

---
+++ chapter4/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main(void) {
+    char first_name[30];
+    char second_name[30];
+    printf("input first name:\n");
+    scanf("%s",first_name);
+    printf("input second name:\n");
+    scanf("%s",second_name);
+    printf("%s,%s\n", first_name,second_name);
+
+}
